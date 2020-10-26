f6a5045fd83a1fd2f61460a6ff8dcd718d1cb172
jiangybao <jiangyubao@gmail.com>
2020-10-26 17:42:02 +0800

    修复编译的bug

--- build_android_release.sh
+++ build_android_release.sh
@@ -3,6 +3,6 @@
 # 参数2：二进制存放目录
 echo "尝试生成flutter_umpush插件内容"
 # 友盟插件的包名是动态的，需要额外调用一个程序来生成
-cd ../flutter_umpush && dart bin/create.dart com.itou.yun.cordova.xiaodian && cd ../recharge_card
-cd ../flutter_wechat_plugin && dart bin/create.dart com.itou.yun.cordova.xiaodian && cd ../recharge_card
+cd ../flutter_umpush && dart bin/create.dart com.itou.yun.cordova.xiaodian && cd ../flutter_smzg
+cd ../flutter_wechat_plugin && dart bin/create.dart com.itou.yun.cordova.xiaodian && cd ../flutter_smzg
 ~/bin/flutter_build_android_release.sh 小店总管安卓版 /Users/jiangyb/Documents/app_deploy
